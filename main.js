c6225b07b9be4a510fa8a6cecd8f96df334d17d6
Faiaz Ben Reza <faiazbenreza@gmail.com>
2026-06-12 00:27:50 +0600

    12-6-2026 update

--- main.js
+++ main.js
@@ -115,8 +115,8 @@
    */
   on('click', '.mobile-nav-toggle', function(e) {
     select('#navbar').classList.toggle('navbar-mobile')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
+    this.classList.toggle('fa-bars')
+    this.classList.toggle('fa-xmark')
   })
 
   /**
@@ -140,8 +140,8 @@
       if (navbar.classList.contains('navbar-mobile')) {
         navbar.classList.remove('navbar-mobile')
         let navbarToggle = select('.mobile-nav-toggle')
-        navbarToggle.classList.toggle('bi-list')
-        navbarToggle.classList.toggle('bi-x')
+        navbarToggle.classList.toggle('fa-bars')
+        navbarToggle.classList.toggle('fa-xmark')
       }
       scrollto(this.hash)
     }
@@ -173,13 +173,6 @@
       backDelay: 2000
     });
   }
-
-  /**
-   * Initiate portfolio lightbox 
-   */
-  const portfolioLightbox = GLightbox({
-    selector: '.portfolio-lightbox'
-  });
 
   /**
    * Testimonials slider
